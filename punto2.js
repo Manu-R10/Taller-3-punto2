8dc14f4ed967fe16b1b557f37d775012b538266a
taller3 punto 2 <manuelraigosa09@gmail.com>
2025-05-24 22:06:37 -0500

    comentarios 2

--- punto2.js
+++ punto2.js
@@ -1,22 +1,22 @@
-let tiempo_limite = 180;
-let advertencias = 0;
-let advertencia_maximas = 3;
+let tiempo_limite = 180; // variable que cuenta el límite máximo de tiempo permitido
+let advertencias = 0; // variable que cuenta las advertencias acumuladas
+let advertencia_maximas = 3; // Variable de Advertencias necesarias para detener el sistema
 
 do {
-    let tiempo_uso = parseInt(prompt("Ingrese el tiempo de uso del agua en segundos:"), 10);
-        tiempo_uso;
-    if (tiempo_uso > tiempo_limite) {
+    let tiempo_uso = parseInt(prompt("Ingrese el tiempo de uso del agua en segundos:"), 10); // Solicita y convierte el tiempo ingresado, el 10 indica que el número ingresado debe interpretarse como decimal
+
+    if (tiempo_uso > tiempo_limite) { // Si supera el límite, se registra una advertencia
         advertencias++;
-        alert("Advertencia,excediste el tiempo de uso.");
+        alert("Advertencia,excediste el tiempo de uso."); // Muestra la advertencia 
     } else {
-        alert("¡Bien! Estás ahorrando agua.");
+        alert("¡Bien! Estás ahorrando agua."); // Mensaje de ahorro si está dentro del límite
     }
 
-    if (advertencias >= advertencia_maximas) {
-        alert("Se ha superado las 3 advertencias maximas. Sistema detenido.");
-        break;
+    if (advertencias >= advertencia_maximas) { // Si llega al máximo de advertencias, detiene el sistema
+        alert("Se ha superado las 3 advertencias maximas. Sistema detenido."); // imprime alerta
+        break; // Sale del bucle
     }
 
-} while (advertencias < advertencia_maximas);
+} while (advertencias < advertencia_maximas); // Repite hasta que se alcance el máximo de advertencias
 
-alert("Monitoreo de ahorro de agua finalizado.");
+alert("Monitoreo de ahorro de agua finalizado."); // imprime Mensaje final cuando el sistema se para
